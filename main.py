d04dcf2f63cbd99116b55b97e80085eb9664b073
Sampaio Nelsandro <nelsandro.sampaio@nokia.com>
2024-11-20 14:23:30 +0100

    Bearer authentication added

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-"""
-
-
-Resources:
- SHeety - https://sheety.co/
-
-"""
-
 import os
 import requests
 import datetime as dt
 from math import floor
 
-app_id = os.environ.get("APP_NUTRITIONIX")
-api_key = os.environ.get("API_KEY_NUTRITIONIX")
-public_key = os.environ.get("API_PUBLIC_KEY_SHEETY")
+APP_ID = os.environ.get("APP_NUTRITIONIX")
+API_KEY = os.environ.get("API_KEY_NUTRITIONIX")
+PUBLIC_KEY = os.environ.get("API_PUBLIC_KEY_SHEETY")
+BEARER_TOKEN_SHEET = os.environ.get("BEARER_TOKEN_SHEET")
 NUTRITION_ENDPOINT = "https://trackapi.nutritionix.com"
 NATURAL_EXERCISE_RESOURCE = "v2/natural/exercise"
-sheety_endpoint = f"https://api.sheety.co/{public_key}/myWorkouts/page1"
+SHEETY_ENDPOINT = f"https://api.sheety.co/{PUBLIC_KEY}/myWorkouts/page1"
 
 
 headers = {
-    "x-app-id": app_id,
-    "x-app-key": api_key
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+sheety_headers = {
+    "Authorization": f"Bearer {BEARER_TOKEN_SHEET}"
 }
 query_str = input("What have you done?")
 parameters_req = {"query": query_str}
@@ -31,7 +28,7 @@ result = response.json()
 # Google Sheet
 today_date = dt.datetime.now().strftime("%d/%m/%Y")
 now_time = dt.datetime.now().strftime("%X")
-rows = []
+
 for exe in result["exercises"]:
     hour = floor(float(exe['duration_min'] / 60))
     minutes = floor(float(exe['duration_min'] % 60))
@@ -43,9 +40,6 @@ for exe in result["exercises"]:
             "duration": f"{hour}h:{minutes}m"
         }
     }
-    sheet_response = requests.post(url=sheety_endpoint, json=data)
-    print(sheet_response)
-
-
-
-
+    sheet_response = requests.post(url=SHEETY_ENDPOINT, json=data, headers=sheety_headers)
+    if str(sheet_response.status_code) == "200":
+        print("Data sent successfully")
